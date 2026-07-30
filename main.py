fe1b55b49d6b81dca7b4f1329e1f2f5139330fb2
AkshatKumarSingh001 <akshatsingh360@gmail.com>
2026-07-31 00:51:47 +0530

    Data Transformation completed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from Network_Security.components.data_ingestion import DataIngestion
 from Network_Security.components.data_validation import DataValidation, DataValidationConfig
+from Network_Security.components.data_transformation import DataTransformation
 from Network_Security.exception.exception import NetworkSecurityException
 from Network_Security.logging.logger import logging
-from Network_Security.entity.config_entity import DataIngestionConfig
+from Network_Security.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from Network_Security.entity.config_entity import TrainingPipelineConfig
 from push_data import push_data_to_mongodb
 import sys
@@ -25,6 +26,12 @@ if __name__ == "__main__":
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data Validation Completed.")
         print(data_validation_artifact)
+        data_transformation_config = DataTransformationConfig(training_pipeline_config)
+        logging.info("Data Transformation Started.")
+        data_transformation = DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("Data Transformation Completed.")
 
 
         try:
